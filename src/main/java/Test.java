c3b3da2ac49145b2fd5617b42447b352287d8805
Lily <lili.korgik@gmail.com>
2023-08-09 11:02:23 +0300

    Println and Print methods

---
+++ src/main/java/Test.java
@@ -0,0 +1,37 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tell me why");
+        System.out.println("Ain't nothin' but a heartache");
+
+        System.out.print("I believe i can fly");
+        System.out.println(" i believe i can touch the sky");
+
+        byte oneByte = 1;
+        short twoByte = 2;
+        int fourByte = 4;
+        long eightByte = 8;
+
+        float fourByteWithDot = 4.89f;
+        double eightByteWithDot = 8.38;
+
+        char oneByteCharter = 36;
+
+        boolean OneByteFalseOrTreu = true;
+
+        System.out.println (oneByte);
+        System.out.println (twoByte);
+        System.out.println (fourByte);
+        System.out.println (eightByte);
+
+        System.out.println (fourByteWithDot);
+        System.out.println (eightByteWithDot);
+
+        System.out.println (oneByteCharter);
+
+        System.out.println (OneByteFalseOrTreu);
+
+    }
+
+}
